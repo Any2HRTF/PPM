40481553e4857f68727501cadbfb5bbd0f58b0da
Florian Pausch <florian.pausch@oeaw.ac.at>
2022-08-03 09:49:17 +0200

    DOCS: minor changes in comments

--- hausdorff_dist.m
+++ hausdorff_dist.m
@@ -3,6 +3,7 @@ function hd = hausdorff_dist(P,Q)
 % P and Q in Euclidian space [1, 2]. Function adapted from [3].
 %
 %   Usage: 
+%
 %     hd = hausdorff_dist(P,Q)
 %
 %   Input parameters:
@@ -14,7 +15,7 @@ function hd = hausdorff_dist(P,Q)
 %
 %     hd : Hausdorff distance between the two sets of points P and Q.
 %
-% [1] Pompeiu, D., Sur la continuit´e des fonctions de variables complexes 
+% [1] Pompeiu, D., Sur la continuité des fonctions de variables complexes 
 %     (These), Gauthier-Villars, Paris, 1905; Ann.Fac.Sci.de Toulouse, 7 
 %     (1905), 264–315.
 % [2] Hausdorff, F., Set Theory, Chelsea Publishing Company, New York, 1957
@@ -27,7 +28,7 @@ function hd = hausdorff_dist(P,Q)
 
 dist = zeros(size(P,1),1);
 for p = 1:size(P,1)
-    % calculate the minimum distance from points in P to Q
+    % Calculate the minimum distance from points in P to Q
     minP = min(sum(bsxfun(@minus,P(p,:),Q).^2, 2));
     dist(p,1) = minP;
 end
