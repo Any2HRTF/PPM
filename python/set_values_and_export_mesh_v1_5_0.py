40ca0e4d958ad50f47c92a91092a049f4fd41e50
Florian Pausch <florian.pausch@oeaw.ac.at>
2022-08-09 10:16:23 +0200

    DOCS: updated function description

--- python/set_values_and_export_mesh_v1_5_0.py
+++ python/set_values_and_export_mesh_v1_5_0.py
@@ -5,7 +5,7 @@
 # Matlab-function descriptions for further information on its use. 
 #
 # Related Python function:
-# get_values_and_export_mesh_v1_4_0.py
+# get_values_and_export_mesh_v1_5_0.py
 #
 # Related Matlab functions:
 # ppm_demo, ppm_get_values, ppm_set_values
