2c9f8bff47507d1544e8367c73e7b63e416786ba
Felix Perfler <me@felixperfler.com>
2023-03-29 15:11:02 +0200

    python 3.10

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     license='The MIT License',
     packages=find_packages(),
     include_package_data=True,
-    python_requires='>3.7',
+    python_requires='>3.10',
     install_requires=[
         "bpy",
         "numpy",
