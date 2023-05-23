bc9b1a17acdca8f6f55d241f4f5ae050c3da0007
Florian Pausch <florian.pausch@oeaw.ac.at>
2023-05-23 09:49:12 +0200

    FEAT: now includes calculation of both directed minimum pointwise distances, and the Hausdorff distance

--- hausdorff_dist.m
+++ hausdorff_dist.m
@@ -1,20 +1,24 @@
-function hd = hausdorff_dist(P,Q)
-%hausdorff_dist - Calculate the minimum pointwise distance between two sets 
-% of points P and Q in Euclidian space [1, 2]. The directed Hausdorff
-% distance corresponds to the maximum of hd. Function adapted from [3].
+function [dist_dir1, dist_dir2, hd] = hausdorff_dist(P,Q)
+%hausdorff_dist - Calculate the directed minimum pointwise distance between  
+% two sets of points P and Q in Euclidian space [1, 2]. The Hausdorff
+% distance corresponds to the larger supremum of dist_dir1 and dist_dir2. 
+% Function adapted from [3].
 %
 %   Usage: 
 %
-%     hd = hausdorff_dist(P,Q)
+%     [dist_dir1, dist_dir2, hd] = hausdorff_dist(P,Q)
 %
 %   Input parameters:
 %
-%     P  : Set of points P.
-%     Q  : Set of points Q. 
+%     P : Set of points P [double]
+%     Q : Set of points Q [double] 
 %
 %   Output parameters:
 %
-%     hd : Hausdorff distance between the two sets of points P and Q.
+%     dist_dir1 : minimum distances between each point in P to Q
+%     dist_dir2 : minimum distances between each point in Q to P
+%     hd        : Hausdorff distance, i.e. larger supremum of 
+%                 dist_dir1 and dist_dir2
 %
 % [1] Pompeiu, D., Sur la continuité des fonctions de variables complexes 
 %     (These), Gauthier-Villars, Paris, 1905; Ann.Fac.Sci.de Toulouse, 7 
@@ -26,11 +30,20 @@ function hd = hausdorff_dist(P,Q)
 
 % #Author: Florian Pausch (2022)   
 
-dist = zeros(size(P,1),1);
+% Calculate the minimum pointwise distance between P and Q
+dist_dir1 = zeros(size(P,1),1);
 for p = 1:size(P,1)
-    % Calculate the minimum distance from points in P to Q
-    minP = min(sum(bsxfun(@minus,P(p,:),Q).^2, 2));
-    dist(p,1) = minP;
+    dist_dir1(p) = min(sum(bsxfun(@minus,P(p,:),Q).^2, 2));
 end
+dist_dir1 = sqrt(dist_dir1);
 
-hd = sqrt(dist);
+% Calculate the minimum pointwise distance between Q and P
+dist_dir2 = zeros(size(Q,1),1);
+for q = 1:size(Q,1)
+    dist_dir2(q) = min(sum(bsxfun(@minus,Q(q,:),P).^2, 2));
+end
+dist_dir2 = sqrt(dist_dir2);
+
+% Calculate Pompeiu-Hausdorff distance
+hd = max([max(dist_dir1), max(dist_dir2)]);
+
