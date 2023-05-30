91a6286d49978297a9520fcce3d8fd38f7b40a00
Felix Perfler <me@felixperfler.com>
2023-05-30 21:53:14 +0200

    added more possibilities to change parameters

--- ppm/core_class.py
+++ ppm/core_class.py
@@ -11,6 +11,94 @@ import bpy
 import bmesh
 import mathutils
 
+
+def euler_to_quaternion(euler_matrix:np.array, sequence:str='ZYX') -> np.array:
+    """Transforms a rotation matrix into a quaternion.
+
+    Parameters
+    ----------
+    euler_matrix : np.array
+        Rotation matrix.
+    sequence : str
+        Rotation sequence. 
+        "ZYX" (default) | "ZYZ" | "ZXY" | "ZXZ" | "YXY" | "YZX" | "YXZ" | "YZY" | "XYX" | "XYZ" | "XZX" | "XZY"
+
+    Returns
+    -------
+    np.array
+        Quaternion.
+    """
+    quaternion = np.zeros((4,))
+
+    if sequence.lower() == 'zyx':
+        c_1 = np.cos(euler_matrix[0]*0.5)
+        s_1 = np.sin(euler_matrix[0]*0.5)
+        c_2 = np.cos(euler_matrix[1]*0.5)
+        s_2 = np.sin(euler_matrix[1]*0.5)
+        c_3 = np.cos(euler_matrix[2]*0.5)
+        s_3 = np.sin(euler_matrix[2]*0.5)
+
+        quaternion[0] = c_1*c_2*c_3 + s_1*s_2*s_3
+        quaternion[1] = c_1*c_2*s_3 - s_1*s_2*c_3
+        quaternion[2] = c_1*s_2*c_3 + s_1*c_2*s_3
+        quaternion[3] = s_1*c_2*c_3 - c_1*s_2*s_3
+
+    elif sequence.lower() == 'zyz':
+        t_1 = euler_matrix[0]*0.5
+        t_2 = euler_matrix[1]*0.5
+        t_3 = euler_matrix[2]*0.5
+
+        quaternion[0] = np.cos(t_2)*np.cos( t_1 + t_3)
+        quaternion[1] = np.sin(t_2)*np.sin(-t_1 + t_3)
+        quaternion[2] = np.sin(t_2)*np.cos(-t_1 + t_3)
+        quaternion[3] = np.cos(t_2)*np.sin( t_1 + t_3)
+
+    elif sequence.lower() == 'zxy':
+        # TODO
+        raise NotImplementedError("Sequence 'ZXY' not implemented yet.")
+
+    elif sequence.lower() == 'zxz':
+        # TODO
+        raise NotImplementedError("Sequence 'ZXZ' not implemented yet.")
+
+    elif sequence.lower() == 'yxy':
+        # TODO
+        raise NotImplementedError("Sequence 'YXY' not implemented yet.")
+    
+    elif sequence.lower() == 'yzx':
+        # TODO
+        raise NotImplementedError("Sequence 'YZX' not implemented yet.")
+    
+    elif sequence.lower() == 'yxz':
+        # TODO
+        raise NotImplementedError("Sequence 'YXZ' not implemented yet.")
+    
+    elif sequence.lower() == 'yzy':
+        '''Kanye ass sequence'''
+        # TODO
+        raise NotImplementedError("Sequence 'YZY' not implemented yet.")
+    
+    elif sequence.lower() == 'xyx':
+        # TODO
+        raise NotImplementedError("Sequence 'XYX' not implemented yet.")
+    
+    elif sequence.lower() == 'xyz':
+        # TODO
+        raise NotImplementedError("Sequence 'XYZ' not implemented yet.")
+    
+    elif sequence.lower() == 'xzx':
+        # TODO
+        raise NotImplementedError("Sequence 'XZX' not implemented yet.")
+    
+    elif sequence.lower() == 'xzy':
+        # TODO
+        raise NotImplementedError("Sequence 'XZY' not implemented yet.")
+
+    else:
+        raise ValueError(f"Sequence '{sequence}' not supported.")
+
+    return quaternion
+
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 class PPM():
@@ -87,13 +175,60 @@ class PPM():
                             for axis_name, axis in type.items():
                                 self.__parameters[parameter_name][point_name][type_name][axis_name] = axis
 
-    def set_parameter(self, parameter, point, type, axis, value):
+    def set_parameter(self, parameter:str, parameter_type:str, value:tuple, point=None, axis=None):
+
+        parameter_type = parameter_type.lower().capitalize()
+        axis = axis.upper() if axis != None else None
+        point = point.lower().capitalize() if point != None else None
+        
+        if parameter_type not in ['Shape_key', 'Scale', 'Rotation', 'Location']:
+            raise Exception('parameter_type must be one of Shape_key, Scale, Rotation, Location')
+        
         if point == 'Shape_key':
-            self.__parameters[parameter][point] = value
-        elif type == 'Scale' and parameter != 'Size':
-            self.__parameters[parameter][point][type] = value
+            if len(value) > 1:
+                raise Exception('value must be a single float value')
+            self.__parameters[parameter][parameter_type] = value
+        elif parameter_type == 'Scale' and parameter != 'Size':
+            if len(value) > 1:
+                raise Exception('value must be a single float value')
+            self.__parameters[parameter]['Bendy'][parameter_type] = value
         else:
-            self.__parameters[parameter][point][type][axis] = value
+            if point not in ['Start', 'End'] and parameter != 'Size':
+                raise Exception('point must be one of Start, End')
+            else:
+                point = 'Bendy'
+            
+            if len(value) != len(axis):
+                raise Exception('value and axis must have the same number of elements')
+
+            if parameter_type == 'Location':
+                for a in axis:
+                    if a not in ['X', 'Y', 'Z']:
+                        raise Exception('axis must be X, Y, Z')
+                for i in range(len(value)):
+                    self.__parameters[parameter][point][parameter_type][axis[i]] = value[i]
+            
+            elif parameter_type == 'Rotation':
+                # rotation defined in euler angles
+                if len(axis) == 3:
+                    for a in axis:
+                        if a not in ['X', 'Y', 'Z']:
+                            raise Exception('axis must be X, Y, Z')
+                    
+                    quaternion = euler_to_quaternion(value, sequence=axis)
+                    
+                    self.__parameters[parameter][point][parameter_type]['W'] = quaternion[0]
+                    self.__parameters[parameter][point][parameter_type]['X'] = quaternion[1]
+                    self.__parameters[parameter][point][parameter_type]['Y'] = quaternion[2]
+                    self.__parameters[parameter][point][parameter_type]['Z'] = quaternion[3]
+
+                elif len(axis) == 4:
+                    if not all(elem in axis for elem in ['W', 'X', 'Y', 'Z']):
+                        raise Exception('axis must contain W, X, Y, Z')
+
+                    for i in range(len(value)):
+                        self.__parameters[parameter][point][parameter_type][axis[i]] = value[i]
+
 
     def __load_parameters_from_csv(self, csv_file=None) -> dict:
 
