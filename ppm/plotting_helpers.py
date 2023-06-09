720b73e8d614631e06b93c5643b47c0a146fe1cf
Felix Perfler <me@felixperfler.com>
2023-06-09 21:58:57 +0200

    made plotting much faster

--- ppm/plotting_helpers.py
+++ ppm/plotting_helpers.py
@@ -1,6 +1,8 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from mpl_toolkits.axes_grid1 import ImageGrid
+from mpl_toolkits import mplot3d
 import numpy as np
 from .math_helpers import minimal_distances
 
@@ -29,18 +31,11 @@ def plot_distances(P, Q):
     else:
         raise TypeError('Q must be of type PPM or np.ndarray')
     
-    hs_dist_exact = minimal_distances(P_points, Q_points)
-
-    # reduce number of points to 5000
-    if P_points.shape[0] > 5000:
-        P_points = P_points[np.random.choice(P_points.shape[0], 5000, replace=False), :]
-    if Q_points.shape[0] > 5000:
-        Q_points = Q_points[np.random.choice(Q_points.shape[0], 5000, replace=False), :]
-
     hs_dist = minimal_distances(P_points, Q_points)
+    hs_normalized = (hs_dist - np.min(hs_dist)) / (np.max(hs_dist) - np.min(hs_dist))
 
     ax1 = plt.subplot(121)
-    ax1.hist(hs_dist_exact,  bins=100, orientation="horizontal", color = "orange", lw=0, histtype='stepfilled'  )
+    ax1.hist(hs_dist,  bins=100, orientation="horizontal", color = "orange", lw=0, histtype='stepfilled'  )
     ax1.set_ylabel('Distance (mm)')
     ax1.set_ylim(0, np.max(hs_dist))
     ax1.xaxis.set_major_locator(ticker.NullLocator())
@@ -50,14 +45,21 @@ def plot_distances(P, Q):
     ax1.spines['bottom'].set_visible(False)
     ax1.set_yticks([0,1,max(hs_dist)])
 
-    C = 255 - np.round((hs_dist - np.min(hs_dist)) / (np.max(hs_dist) - np.min(hs_dist)) * 255).astype(int)
 
     ax2 = plt.subplot(122, projection='3d')
-    ax2.scatter(P_points[:,0],P_points[:,1],P_points[:,2], c=C, cmap='gist_heat', alpha=0.1)
-    ax2.scatter(Q_points[:,0],Q_points[:,1],Q_points[:,2], color='black', alpha=0.05)
+    Q_plot = mplot3d.art3d.Poly3DCollection([Q_points], facecolor='black', alpha=0.1)
+    ax2.add_collection3d(Q_plot)
+    
+    # P_plot = mplot3d.art3d.Poly3DCollection([P_points], facecolor='red', alpha=0.1)
+    # ax2.add_collection3d(P_plot)
+
+    ax2.scatter(P_points[:,0], P_points[:,1], P_points[:,2], c=hs_normalized, cmap='jet', s=1, alpha=0.5)
+    
+    scale = P_points.flatten()
+    ax2.auto_scale_xyz(scale, scale, scale)
     ax2.axis('off')
 
-    # add title to show mean, variance and median in LaTeX
-    plt.suptitle(r'$\mu=%.2f,\ \sigma=%.2f,\ \mathrm{median}=%.2f$' %(np.mean(hs_dist_exact), np.std(hs_dist_exact), np.median(hs_dist_exact)))
+
+    plt.suptitle(r'$\mu=%.2f,\ \sigma=%.2f,\ \mathrm{median}=%.2f$' %(np.mean(hs_dist), np.std(hs_dist), np.median(hs_dist)))
 
     plt.show()
