C implementation, copy fixed, both direction calculation added

Blender Add-on/Hausdorff_PT_op.py
@@ -1,169 +1,110 @@
 import bpy
 import numpy as np
-
 import time
+import ctypes as cts
+import sys
 
 
-class VisualizeHausdorff(bpy.types.Operator):
 
-    bl_idname = "object.visualize_hausdorff"
+class VisualizeDistance(bpy.types.Operator):
+    """
+    A class used to visualize either the pointwise minimal distance
+    or the dice index
+    ...
 
-    bl_label = "Hausdorff Visualization"
+    Methods
+    -------
+    poll(cls, context)
+        Bouncer function which decides whether the execute function is called
+    execute(self, context)
+        calculates the chosen distance, stores the relevant statistical measurements
+        and visualizes the data
+    
+    """
 
-    bl_description = "Calculate and visualize the Hausdorff distance to a reference object"
+    bl_idname = "object.visualize_distance"
+    bl_label = "Distance Visualization"
+    bl_description = "Calculate and visualize the chosen distance to/from a reference object"
 
 
 
     @classmethod
-
     def poll(cls, context):
-
         obj = context.object
-
-        if obj is not None:
-
-            if obj.mode == "OBJECT":
-
-                return True
-
-        return False
-
+        if obj is not None and obj.mode == "OBJECT":
+            return True
+        else:
+            return False
     
 
     def execute(self, context):
         
-        start_time=time.time()
-        
         obj1 = context.view_layer.objects.active
-        
-        obj1_copy = create_mesh_copy(obj1)
-        
-        check_if_nomats(obj1_copy)
-        
-        set_use_nodes_False(obj1_copy)
-        
-        check_if_nocols(obj1_copy)
 
-        obj2= context.scene.objects[context.scene.theReferenceObject]         #setting up reference object
+        check_if_nomats(obj1)
+        set_use_nodes_False(obj1)
+        check_if_nocols(obj1)
 
-        obj2_copy = create_mesh_copy(obj2)
-        
-        #creating and applying decimation modifier
-        if len(obj1_copy.data.vertices)>=30000:
-            modifier = obj1_copy.modifiers.new(name="Decimate", type='DECIMATE')
-            modifier.ratio = 0.3  # Set the desired decimation ratio (0.5 = 50% reduction)
+
+        obj2= context.scene.objects[context.scene.Reference]         #setting up reference object
+        color_map = obj1.data.vertex_colors.active.data
+        MW2 = obj2.matrix_world
+        MW1 = obj1.matrix_world
+        P= np.array([MW1 @ vert.co for vert in obj1.data.vertices ])
+        Q= np.array([MW2 @ vert.co for vert in obj2.data.vertices ])
+
+
+
+        # P -> Q
+        #calculate Hausdorff Distance
+        hausdorff, dealloc_array  = hausdorff_distance(P,Q)
             
-            bpy.context.view_layer.objects.active = obj1_copy
-            bpy.ops.object.modifier_apply(modifier=modifier.name)
+
+
+        #store statisctical measurements
+        bpy.ops.object.mode_set(mode='VERTEX_PAINT')
+        hausdorff_item = context.scene.distances.add()
+        hausdorff_item.mean_PQ = np.round(np.mean(hausdorff), decimals=2)
+        print("Mean: ", hausdorff_item.mean_PQ)
+        hausdorff_item.median_PQ = np.round(np.median(hausdorff), decimals=2)
+        print("Median: ", hausdorff_item.median_PQ)
+        hausdorff_item.max_PQ = np.round(np.max(hausdorff), decimals=2)
+        print("Maximum: ", hausdorff_item.max_PQ)
+        hausdorff_item.min_PQ = np.round(np.min(hausdorff), decimals=2)
+        print("Minimum: ", hausdorff_item.min_PQ)
+
+        hausdorff_trans=list((hausdorff - np.mean(hausdorff))*(1/(np.mean(hausdorff)+0.1))+1)
+        #free memory
+        dealloc_array(hausdorff)
+
+
+        # Q -> P
+        #calculate Hausdorff Distance
+        hausdorff, dealloc_array  = hausdorff_distance(Q,P)
         
-        if len(obj2_copy.data.vertices)>=30000:
-            modifier = obj2_copy.modifiers.new(name="Decimate", type='DECIMATE')
-            modifier.ratio = 0.3  # Set the desired decimation ratio (0.5 = 50% reduction)
-
-            bpy.context.view_layer.objects.active = obj2_copy
-            bpy.ops.object.modifier_apply(modifier=modifier.name)
-
-        color_map = obj1_copy.data.vertex_colors.active.data
-        
-        
-
-        MW2 = obj2_copy.matrix_world
-
-        MW1 = obj1_copy.matrix_world
-
-    
-
-        #setting up P and Q
-
-        P= np.array([MW1 @ vert.co for vert in obj1_copy.data.vertices ])
-
-        Q= np.array([MW2 @ vert.co for vert in obj2_copy.data.vertices ])
-
-    
-
-        #calculating hausdorff distance of each point
-
-        #hd=cdist(P,Q)
-        #hausdorff=np.min(hd,axis=1)
-
-        hausdorff=hausdorff_distance(P,Q)
-
-        end_time=time.time()
-
-        #hausdorff_l=list(hausdorff)
-
-        hausdorff_trans=list((hausdorff - np.mean(hausdorff))*(1/np.mean(hausdorff))+1)
-
-        # print("Hausdorf calculated")
-
-        print('Midway Time =', end_time-start_time)
-        
-        print("Maximum: ", max(hausdorff))
-
-        print("Minimum: ", min(hausdorff))
-
-        print("Mean: ", np.mean(hausdorff))
-    
-        print("Median: ", np.median(hausdorff))
-
-        #print("Mean trans: ", np.mean(hausdorff_trans))
+        #store statisctical measurements
+        hausdorff_item.mean_QP = np.round(np.mean(hausdorff), decimals=2)
+        print("Mean: ", hausdorff_item.mean_QP)
+        hausdorff_item.median_QP = np.round(np.median(hausdorff), decimals=2)
+        print("Median: ", hausdorff_item.median_QP)
+        hausdorff_item.max_QP = np.round(np.max(hausdorff), decimals=2)
+        print("Maximum: ", hausdorff_item.max_QP)
+        hausdorff_item.min_QP = np.round(np.min(hausdorff), decimals=2)
+        print("Minimum: ", hausdorff_item.min_QP)
+        #free memory
+        dealloc_array(hausdorff)
 
 
 
 
-
-        # get point cloud data
-
-        #P = get_points_from_obj(obj1)
-
-        #Q = get_points_from_obj(obj2)
-
-        #hausdorff_direction_1 = hausdorff_distance(P, Q)
-
-        #hausdorff_direction_2 = hausdorff_distance(Q, P)
-
-        
-
-        # We should do this to get the generalized Hausdorff distance as we are interested in the worst case
-
-        # if hausdorff_direction_1.max() > hausdorff_direction_2.max():
-
-        #     hausdorff = hausdorff_direction_1
-
-        # else:
-
-        #     hausdorff = hausdorff_direction_2
-
-        
-        
-        #print("Maximum: ", max(hausdorff))
-
-        #print("Mean: ", np.mean(hausdorff))
-
-        #print("Median: ", np.median(hausdorff))
-
-        #print("Minimum: ", min(hausdorff))
-
-
-
-        hausdorff_trans=list((hausdorff - np.mean(hausdorff))*(1/np.mean(hausdorff))+1)
-
-
-
-        
 
         # setting up color array
-
-        color_array = np.zeros(len(obj1_copy.data.vertices) * 4, dtype=np.float32)
-
-        color_array.shape = (len(obj1_copy.data.vertices), 4)
-
+        color_array = np.zeros(len(obj1.data.vertices) * 4, dtype=np.float32)
+        color_array.shape = (len(obj1.data.vertices), 4)
     
 
         # iterating through vertices and setting colors
-
-        for i, vert in enumerate(obj1_copy.data.vertices):
-
+        for i, vert in enumerate(obj1.data.vertices):
             if hausdorff_trans[i] <= 1:
 
                 color_array[i] = [0, 0, 1, 1]
@@ -184,232 +125,80 @@ class VisualizeHausdorff(bpy.types.Operator):
 
         # setting colors for the object
 
-        for loop in obj1_copy.data.loops:
+        for loop in obj1.data.loops:
 
             color_map[loop.index].color=list(color_array[loop.vertex_index])
 
-            #print('colored',loop.index,'/',len(obj1.data.loops))
 
-        bpy.data.objects.remove(obj2_copy, do_unlink=True)
+        obj1.select_set(True)
 
-        #bpy.data.objects.remove(obj1_copy, do_unlink=True)
+        bpy.context.view_layer.objects.active = obj1
 
-        obj1_copy.select_set(True)
-
-        bpy.context.view_layer.objects.active = obj1_copy
-
-        bpy.ops.object.mode_set(mode='VERTEX_PAINT')
-
-        hausdorff_item = context.scene.hausdorff.add()
-
-        hausdorff_item.mean = np.round(np.mean(hausdorff), decimals=2)
-
-        hausdorff_item.median = np.round(np.median(hausdorff), decimals=2)
-
-        hausdorff_item.max = np.round(np.max(hausdorff), decimals=2)
-
-        hausdorff_item.min = np.round(np.min(hausdorff), decimals=2)
-
-        end_time2=time.time()
-
-        print("End time = ", end_time2-start_time)
 
         return {'FINISHED'}
 
     
-class HausdorffProperty(bpy.types.PropertyGroup):
-    mean: bpy.props.FloatProperty(name="Mean", default =0.0)
-    median: bpy.props.FloatProperty(name="Median",default =0.0 )
-    max: bpy.props.FloatProperty(name="Maximum",default =0.0 )
-    min: bpy.props.FloatProperty(name="Minimum",default =0.0 )
+class DistanceProperty(bpy.types.PropertyGroup):
+    """
+    A class used to init the relevant statistical distances
+    ...
 
-
-
-
-
-class ResetColors(bpy.types.Operator):
-
-    bl_idname = "object.reset_colors"
-
-    bl_label = "Reset colors"
-
-    bl_description = "Resets colors after visualizing the Hausdorff distance"
-
-
-
-    @classmethod
-
-    def poll(cls, context):
-
-        obj = context.object
-
-        if obj is not None:
-
-            if obj.mode == "OBJECT":
-
-                return True
-
-        return False
-
-    
-
-    def execute(self, context):
-
-
-
-        obj1 = context.view_layer.objects.active
-
-        color_map=obj1.data.vertex_colors.active.data
-
-
-
-        for index in range(len(color_map)):
-
-            color_map[index].color=[1,1,1,8]
-
+    Attributes
+    -------
+    mean_PQ   : float
+        the mean distance to Reference
+    median_PQ : float
+        the median distance to Reference
+    max_PQ    : float
+        the max distance to Reference
+    min_PQ    : float
+        the min distance to Reference
+    mean_QP   : float
+        the mean distance from Reference
+    median_QP : float
+        the median distance from Reference
+    max_QP    : float
+        the max distance from Reference
+    min_QP    : float
+        the min distance from Reference
+  
+    """
         
-
-        return {'FINISHED'}
-
+    mean_PQ: bpy.props.FloatProperty(name="Mean_PQ", default =0.0)
+    median_PQ: bpy.props.FloatProperty(name="Median_PQ",default =0.0 )
+    max_PQ: bpy.props.FloatProperty(name="Maximum_PQ",default =0.0 )
+    min_PQ: bpy.props.FloatProperty(name="Minimum_PQ",default =0.0 )
     
-class OutputHausdorff(bpy.types.Operator):
-
-    bl_idname = "object.output_hausdorff"
-
-    bl_label = "Output Hausdorff Distance"
-
-    bl_description = "Output the mean and max Hausdorff distance"
+    mean_QP: bpy.props.FloatProperty(name="Mean_QP", default =0.0)
+    median_QP: bpy.props.FloatProperty(name="Median_QP",default =0.0 )
+    max_QP: bpy.props.FloatProperty(name="Maximum_QP",default =0.0 )
+    min_QP: bpy.props.FloatProperty(name="Minimum_QP",default =0.0 )
 
 
-
-    @classmethod
-
-    def poll(cls, context):
-
-        obj = context.object
-
-        if obj is not None:
-
-            if obj.mode == "OBJECT":
-
-                return True
-
-        return False
-
-    
-
-    def execute(self, context):
-        print("sdfhl")
-        start_time=time.time()
-
-        obj1 = context.view_layer.objects.active
-        obj2= context.scene.objects[context.scene.theReferenceObject] 
-        
-        # creating the meshes
-        obj1_copy = create_mesh_copy(obj1)
-        obj2_copy = create_mesh_copy(obj2)
-
-        #creating and applying decimation modifier
-        if len(obj1_copy.data.vertices)>=30000:
-            modifier = obj1_copy.modifiers.new(name="Decimate", type='DECIMATE')
-            modifier.ratio = 0.3  
-            
-            bpy.context.view_layer.objects.active = obj1_copy
-            bpy.ops.object.modifier_apply(modifier=modifier.name)
-
-        if len(obj2_copy.data.vertices)>=30000:
-            modifier = obj2_copy.modifiers.new(name="Decimate", type='DECIMATE')
-            modifier.ratio = 0.3  
-
-            bpy.context.view_layer.objects.active = obj2_copy
-            bpy.ops.object.modifier_apply(modifier=modifier.name)
-
-        # world matrices
-        MW2 = obj2_copy.matrix_world
-        MW1 = obj1_copy.matrix_world
-
-        # setting up P and Q
-
-        P= np.array([MW1 @ vert.co for vert in obj1_copy.data.vertices ])
-        Q= np.array([MW2 @ vert.co for vert in obj2_copy.data.vertices ])
-
-        # calculation bi-directional hausdorff distance
-       
-        #hd=cdist(P,Q)
-        #hausdorff_direction_1=np.min(hd,axis=1)
-
-        hausdorff_direction_1 = hausdorff_distance(P, Q)
-
-        mid_time1=time.time()
-        print("Mid time 1 =", mid_time1-start_time)
-
-
-        hausdorff_direction_2 = hausdorff_distance(Q, P)
-       
-        #hd2=cdist(Q,P)
-        #hausdorff_direction_2=np.min(hd2,axis=1)
-
-        mid_time2=time.time()
-        print("Mid time 2 =", mid_time2-start_time)
-
-        # We should do this to get the generalized Hausdorff distance as we are interested in the worst case
-        #if hausdorff_direction_1.max() > hausdorff_direction_2.max():
-            #hausdorff = hausdorff_direction_1
-
-       # else:
-           #hausdorff = hausdorff_direction_2
-
-        hausdorff=np.concatenate((hausdorff_direction_1,hausdorff_direction_2))
-
-
-        #printing the results:
-        print("Maximum: ", max(hausdorff))
-        print("Minimum: ", min(hausdorff))
-        print("Mean: ", np.mean(hausdorff))
-        print("Median: ", np.median(hausdorff))
-
-        #updating the hausdorff property in the panel
-        hausdorff_item = context.scene.hausdorff.add()
-        hausdorff_item.mean = np.round(np.mean(hausdorff), decimals=2)
-        hausdorff_item.median = np.round(np.median(hausdorff), decimals=2)
-        hausdorff_item.max = np.round(np.max(hausdorff), decimals=2)
-        hausdorff_item.min = np.round(np.min(hausdorff), decimals=2)
-    
-
-        #deleting the meshes
-        bpy.data.objects.remove(obj2_copy, do_unlink=True)
-        bpy.data.objects.remove(obj1_copy, do_unlink=True)
-
-        end_time = time.time()
-        print("End Time = ",end_time-start_time)
-
-        return {'FINISHED'}
-
-
-
-
-
-
+def np_mat_type(rows, cols, element_type=float):
+    return np.ctypeslib.ndpointer(dtype=element_type, shape=(rows, cols), flags="C_CONTIGUOUS")
 def hausdorff_distance(P,Q):
+    
+    DLL_NAME = "C:/Program Files/Blender Foundation/Blender 3.6/3.6/scripts/addons\Hausdorff/haus_40.{:s}".format("dll" if sys.platform[:3].lower() == "win" else "so")
 
-    #inputs P and Q are arrays of vert coordinates
+    rows0,cols0 = P.shape
+    rows1,cols1 = Q.shape
+    dll = cts.CDLL(DLL_NAME)
+    matrix_func = dll.matrixFunc
+    matrix_func.argtypes = (
+        np_mat_type(rows0, cols0), cts.c_size_t, cts.c_size_t,
+        np_mat_type(rows1, cols1), cts.c_size_t, cts.c_size_t)
+    matrix_func.restype = np_mat_type(rows0, 1)
+    dealloc_array = dll.deallocArray
+    dealloc_array.argtypes = (np_mat_type(rows0, 1),)
+    dealloc_array.restype = None
 
-    dist = np.zeros((P.shape[0], 1))
 
-    for p in range(P.shape[0]):
-
-        # Calculate the minimum distance from points in P to Q
-
-        minP = np.min(np.sum((P[p, :] - Q)**2, axis=1))
-
-        dist[p, 0] = minP
-
-
-
-    hd = np.sqrt(dist)
-
-    return hd
-
+    start = time.time()
+    hausdorff = matrix_func(P, rows0, cols0, Q, rows1, cols1)
+    end = time.time()
+    print(f"Time for Hausdorff: {end - start}")
+    return hausdorff, dealloc_array
 
 
 def check_if_nomats(obj):
@@ -417,7 +206,7 @@ def check_if_nomats(obj):
     
 
     if len(obj.data.materials)==0:
-
+        print("LEN OF DATA IS ZERO")
         new_mat=bpy.data.materials.new('NewMaterial')
 
         obj.data.materials.append(new_mat)
@@ -425,42 +214,13 @@ def check_if_nomats(obj):
     
 
     return {'FINISHED'}
-
-
-
-
-
-
-
 def set_use_nodes_False(obj):
 
     for mats in obj.data.materials:
 
         mats.use_nodes=False
-
-
-
 def check_if_nocols(obj):
 
     if obj.data.vertex_colors.active == None:
 
-        obj.data.vertex_colors.new()
-
-    
-def create_mesh_copy(obj1):
-
-    obj1_copy = obj1.copy()
-
-    obj1_copy.data = obj1.data.copy()
-
-    bpy.context.collection.objects.link(obj1_copy)
-
-    obj1.select_set(False)
-
-    obj1_copy.select_set(True)
-
-    bpy.context.view_layer.objects.active = obj1_copy
-        
-    bpy.ops.object.convert(target='MESH')
-
-    return obj1_copy
+        obj.data.vertex_colors.new()    